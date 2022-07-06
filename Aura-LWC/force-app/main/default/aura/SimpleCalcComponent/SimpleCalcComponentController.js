401c35a472b9e10ea7913d08edf2eddef21bc345
Recep  Eren <eren@C02YT245LVCF-652BA.local>
2022-07-06 13:57:35 -0400

    .

--- Aura-LWC/force-app/main/default/aura/SimpleCalcComponent/SimpleCalcComponentController.js
+++ Aura-LWC/force-app/main/default/aura/SimpleCalcComponent/SimpleCalcComponentController.js
@@ -2,10 +2,77 @@
     sumClientController : function(component, event, helper) {
         var numberX = component.get("v.xnumber");
         var numberY = component.get("v.ynumber");
-        var result = numberX+numberY;
-        console.log(numberX);
-        console.log(numberY);
-        console.log(result);
-        component.set("v.result",result);
-        }
+        // var result = numberX+numberY;
+        // component.set("v.result",result);
+         var serverController = component.get("c.sumServerController"); // reaching Apex class method
+         serverController.setParams( // sending required variables to apex class. (JSON)
+            {
+                "number1":numberX,
+                "number2":numberY
+            });
+
+            serverController.setCallback(this,function(response){ //taking the result back from Apex class
+                console.log(response.getReturnValue());
+                component.set("v.result",response.getReturnValue()); // write the calculated result to component(v.var is in the component)
+            }
+            );
+            $A.enqueueAction(serverController); //must to write to speak with apex server
+
+
+
+        },
+    subClientController : function(component, event, helper) {
+        var numberX = component.get("v.xnumber");
+        var numberY = component.get("v.ynumber");
+        // var result = numberX+numberY;
+        // component.set("v.result",result);
+        var serverController = component.get("c.subServerController"); // reaching Apex class method
+        serverController.setParams( // sending required variables to apex class. (JSON)
+           {
+               "number1":numberX,
+               "number2":numberY
+           });
+
+           serverController.setCallback(this,function(response){ //taking the result back from Apex class
+               console.log(response.getReturnValue());
+               component.set("v.result",response.getReturnValue()); // write the calculated result to component(v.var is in the component)
+           }
+           );
+           $A.enqueueAction(serverController); //must to write to speak with apex server
+
+        },
+
+    mulClientController : function(component, event, helper) {
+        var numberX = component.get("v.xnumber");
+        var numberY = component.get("v.ynumber");
+        var serverController = component.get("c.mulServerController");
+        serverController.setParams(
+            {"number1":numberX,
+             "number2":numberY
+
+        });
+        serverController.setCallback(this,function(response){
+            component.set("v.result",response.getReturnValue());
+        });
+
+        $A.enqueueAction(serverController);
+
+        
+        },
+    divClientController : function(component, event, helper) {
+        var numberX = component.get("v.xnumber");
+        var numberY = component.get("v.ynumber");
+        var serverController = component.get("c.divServerController");
+        serverController.setParams(
+            {"number1":numberX,
+             "number2":numberY
+
+        });
+        serverController.setCallback(this,function(response){
+            component.set("v.result",response.getReturnValue());
+        });
+
+        $A.enqueueAction(serverController);
+       
+        },
 })
