a2656cd8903afd66110e930ae10dda1d6f256a29
receperen <qreceperen@gmail.com>
2022-07-12 18:48:20 -0400

    .

---
+++ ApexProjects/force-app/main/default/aura/Example3/Example3Controller.js
@@ -0,0 +1,8 @@
+({
+    myAction : function(component, event, helper) {
+
+        var target= event.getSource();
+        component.set("v.text", target.get("v.label"));
+
+    }
+})
---
+++ ApexProjects/force-app/main/default/aura/Example4/Example4Controller.js
@@ -0,0 +1,8 @@
+({
+    myAction : function(component, event, helper) {
+
+        var myLabel = component.find("button1").get("v.label");
+        component.set("v.buttonLabel", myLabel)
+
+    }
+})
