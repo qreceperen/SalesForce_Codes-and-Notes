43f27d0f88743c2fff490988c0615441f02daefe
receperen <qreceperen@gmail.com>
2022-07-15 20:29:24 -0400

    .

---
+++ Aura-LWC/force-app/main/default/lwc/example04/example04.js
@@ -0,0 +1,22 @@
+import { LightningElement } from 'lwc';
+
+export default class Example04 extends LightningElement {
+    firstName;
+    lastName;
+
+    changeHandlerFirstName(event){
+        this.firstName = event.target.value;
+    }
+
+
+    changeHandlerLastName(event){
+        this.lastName = event.target.value;
+    }
+    handleClick(){
+
+        alert("Hello " + this.firstName +' ' + this.lastName);
+    }
+
+
+
+}
