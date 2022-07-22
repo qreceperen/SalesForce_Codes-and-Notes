632da32a63e7c9a5b6a180a04d5523e96cc93789
receperen <qreceperen@gmail.com>
2022-07-21 22:52:29 -0400

    Progress Bar is Added

---
+++ Aura-LWC/force-app/main/default/lwc/richTextComponent/richTextComponent.js
@@ -0,0 +1,14 @@
+import { LightningElement,api } from 'lwc';
+
+export default class RichTextComponent extends LightningElement {
+
+    @api fieldValue=" ";
+    @api fieldLabel;
+
+    changeHandle(event){
+        this.fieldValue = event.target.value;
+        // console.log(this.fieldValue);
+
+    }
+
+}
