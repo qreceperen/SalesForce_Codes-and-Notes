ac72ba393aa993246fc643af0998154a205d46b8
receperen <qreceperen@gmail.com>
2022-07-27 23:00:29 -0400

    ,

---
+++ Aura-LWC/force-app/main/default/lwc/wireApex/wireApex.js
@@ -0,0 +1,8 @@
+import { LightningElement, wire } from 'lwc';
+import getAccountList from '@salesforce/apex/AccountController.getAccountList';
+
+export default class WireApex extends LightningElement {
+
+    @wire (getAccountList) wireAccounts
+
+}
